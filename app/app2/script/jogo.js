c3d8e0656411cf3b68c64efed7da5b1a790587ff
CarlosSantos-new <99801172+CarlosSantosweb@users.noreply.github.com>
2023-02-18 16:18:02 -0300

    atualização
    melhorando tempo do gamer

--- app/app2/script/jogo.js
+++ app/app2/script/jogo.js
@@ -4,6 +4,22 @@ var largura = 0
 var vidas = 1
 var tempo = 15
 
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+//Nivel do Gamer
+if(nivel === 'normal'){
+    //1500
+    criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+    //1000
+    criaMosquitoTempo = 1000
+} else if(nivel === 'jhonwick') {
+    //750
+    criaMosquitoTempo = 750
+}
+
 function ajustaTelaGamer() {
     altura = window.innerHeight
     largura = window.innerWidth
